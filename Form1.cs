aeeda748255078768d0ffcd3de6d0d923c734c63
Developonz <zhenya2k04@gmail.com>
2024-05-22 15:11:50 +0800

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace particles
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particles = new List<Particle>();
+
+        List<Emitter> emitters = new List<Emitter>();
+        Emitter emitter;
+
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
+        GravityPoint point1; // добавил поле под первую точку
+        GravityPoint point2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            this.emitter = new Emitter // создаю эмиттер и привязываю его к полю emitter
+            {
+                Direction = 0,
+                Spreading = 10,
+                SpeedMin = 10,
+                SpeedMax = 10,
+                ColorFrom = Color.Gold,
+                ColorTo = Color.FromArgb(0, Color.Red),
+                ParticlesPerTick = 10,
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2,
+            };
+
+            emitters.Add(this.emitter);
+
+            point1 = new GravityPoint
+            {
+                X = picDisplay.Width / 2 + 100,
+                Y = picDisplay.Height / 2,
+            };
+            point2 = new GravityPoint
+            {
+                X = picDisplay.Width / 2 - 100,
+                Y = picDisplay.Height / 2,
+            };
+
+            // привязываем поля к эмиттеру
+            emitter.impactPoints.Add(point1);
+            emitter.impactPoints.Add(point2);
+
+            lblDirection.Text = $"{tbDirection.Value}°";
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                emitter.Render(g); // а тут теперь рендерим через эмиттер
+            }
+
+            picDisplay.Invalidate();
+        }
+
+
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
+
+            point2.X = e.X;
+            point2.Y = e.Y;
+        }
+
+        private void tbDirection_Scroll(object sender, EventArgs e)
+        {
+            emitter.Direction = tbDirection.Value;
+            lblDirection.Text = $"{tbDirection.Value}°";
+        }
+
+        private void tbGraviton_Scroll(object sender, EventArgs e)
+        {
+            point1.Power = tbGraviton.Value;
+        }
+
+        private void tbGraviton2_Scroll(object sender, EventArgs e)
+        {
+            point2.Power = tbGraviton2.Value;
+        }
+    }
+}
