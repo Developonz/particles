c4bb3085eba1ab39f1e79e6dccb767e5b519e2fb
Developonz <zhenya2k04@gmail.com>
2024-05-23 23:24:38 +0800

    create paintCircle

--- Particle.cs
+++ Particle.cs
@@ -23,11 +23,9 @@ namespace particles
             var direction = (double)rand.Next(360);
             var speed = 1 + rand.Next(10);
 
-            // рассчитываем вектор скорости
             SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
             SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
 
-            // а это не трогаем
             Radius = 2 + rand.Next(10);
             Life = 20 + rand.Next(100);
         }
